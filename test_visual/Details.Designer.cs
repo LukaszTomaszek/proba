358d355adca058248f9832841e8feba1ac35dca5
LukaszTomaszek <lu.tomaszek@gmail.com>
2020-04-29 17:45:23 +0200

    test 1745

---
+++ test_visual/Details.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Details
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Details = new System.Windows.Forms.Label();
+            this.dgvRecordsList = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox3 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.txtBox4 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRecordsList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_Details
+            // 
+            this.lbl_Details.AutoSize = true;
+            this.lbl_Details.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_Details.Location = new System.Drawing.Point(84, 133);
+            this.lbl_Details.MinimumSize = new System.Drawing.Size(5, 5);
+            this.lbl_Details.Name = "lbl_Details";
+            this.lbl_Details.Size = new System.Drawing.Size(5, 73);
+            this.lbl_Details.TabIndex = 1;
+            // 
+            // dgvRecordsList
+            // 
+            this.dgvRecordsList.AllowUserToOrderColumns = true;
+            this.dgvRecordsList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRecordsList.Location = new System.Drawing.Point(95, 56);
+            this.dgvRecordsList.Name = "dgvRecordsList";
+            this.dgvRecordsList.Size = new System.Drawing.Size(382, 96);
+            this.dgvRecordsList.TabIndex = 2;
+            this.dgvRecordsList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvRecordsList_CellContentClick);
+            // 
+            // txtName
+            // 
+            this.txtName.AutoSize = true;
+            this.txtName.Location = new System.Drawing.Point(128, 219);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(35, 13);
+            this.txtName.TabIndex = 4;
+            this.txtName.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(128, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(125, 311);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(125, 181);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(35, 13);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "label1";
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(247, 178);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(100, 20);
+            this.txtBox1.TabIndex = 7;
+            // 
+            // txtBox3
+            // 
+            this.txtBox3.Location = new System.Drawing.Point(247, 269);
+            this.txtBox3.Name = "txtBox3";
+            this.txtBox3.Size = new System.Drawing.Size(100, 20);
+            this.txtBox3.TabIndex = 8;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(247, 229);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(100, 20);
+            this.txtBox2.TabIndex = 9;
+            // 
+            // txtBox4
+            // 
+            this.txtBox4.Location = new System.Drawing.Point(247, 311);
+            this.txtBox4.Name = "txtBox4";
+            this.txtBox4.Size = new System.Drawing.Size(100, 20);
+            this.txtBox4.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(218, 12);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // Details
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 667);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtBox4);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox3);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.dgvRecordsList);
+            this.Controls.Add(this.lbl_Details);
+            this.Name = "Details";
+            this.Text = "Details";
+            this.Load += new System.EventHandler(this.Details_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRecordsList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Details;
+        private System.Windows.Forms.DataGridView dgvRecordsList;
+        private System.Windows.Forms.Label txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.TextBox txtBox3;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.TextBox txtBox4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
---
+++ test_visual/Details.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Details : Form
+    {
+        public Details(SzwalniaEntities db, ,String employeeLastName)
+        {
+            InitializeComponent();
+            Pracownicy employee = db.Pracownicy
+                .Where(pracownik => pracownik.Nazwisko == employeeLastName).First();
+
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Details_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvRecordsList_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
